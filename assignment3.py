ca124053b238eb046a53e8a18c8fb6ee044b0aaf
Michael Balcerzak <balcerzak.michael@gmail.com>
2021-04-09 18:56:16 -0400

    updated max value

--- assignment3.py
+++ assignment3.py
@@ -2,73 +2,57 @@ import numpy
 import sys
 import math
 
-
 class td_qlearning:
     alpha = 0.1
     gamma = 0.5
 
-    qValuesWithActions = {"C": [0, 0, 0, 0, 0],
-                          "L": [0, 0, 0, 0, 0],
-                          "R": [0, 0, 0, 0, 0],
-                          "U": [0, 0, 0, 0, 0],
-                          "D": [0, 0, 0, 0, 0]}
+    allTrajectoryIterations = {}
+    allIterationsInOrder = []
 
     reward = 0
 
-    def findingMaxValue(self, state):
-        nextStatesValue = []
-        nextStatesAction = []
+    def findingMaxQValue(self, state, action):
 
-        currentLocation = int(state[0])
-        index = currentLocation-1
+        S_t_index = self.allIterationsInOrder.index(str(state) + "," + action)
+        S_t1 = self.allIterationsInOrder[S_t_index + 1].split(",")[0]
 
-        if currentLocation == 1:
-            nextStatesAction.append('C')
-            nextStatesValue.append(self.qValuesWithActions['C'][index])
+        stateWithActionsQValues = self.allTrajectoryIterations[S_t1]
 
-        if 3 <= currentLocation <= 4:
-            nextStatesValue.append(self.qValuesWithActions['L'][index - 1])
-            nextStatesAction.append('L')
+        maxQValue = -1000000000
+        for qValue in stateWithActionsQValues.values():
+            if maxQValue < int(qValue):
+                maxQValue = qValue
 
-        if 2 <= currentLocation <= 3:
-            nextStatesValue.append(self.qValuesWithActions['R'][index + 1])
-            nextStatesAction.append('R')
-
-        if currentLocation == 3 or currentLocation == 5:
-            nextStatesValue.append(self.qValuesWithActions['U'][index - 2])
-            nextStatesAction.append('U')
-
-        if currentLocation == 1 or currentLocation == 3:
-            nextStatesValue.append(self.qValuesWithActions['D'][index + 2])
-            nextStatesAction.append('D')
-
-        indexMax = nextStatesValue.index(max(nextStatesValue))
-
-        return max(nextStatesValue), nextStatesAction[indexMax]
+        return maxQValue
 
     def __init__(self, trajectory_filepath):
         # trajectory_filepath is the path to a file containing a trajectory through state space
         # Return nothing
 
-        trajectory_filepath_readable = open(trajectory_filepath, "r")
+        trajectory_filename_readable = open(trajectory_filepath, "r")
 
-        stateInAList = trajectory_filepath_readable.read().split("\n")
+        stateInAList = trajectory_filename_readable.read().split("\n")
 
         for stateAndAction in stateInAList:
 
-            if stateAndAction == '':
-                break
+            if stateAndAction == '': break
 
             state = stateAndAction.split(",")[0]
-            action = stateAndAction.split(",")[1]
-            squareLocation = int(state[0]) - 1
 
-            q = self.qvalue(state, action)
-            a = self.policy(state)
+            location = int(state[0])
 
-            self.qValuesWithActions[a][squareLocation] = q
+            if location == 1:
+                self.allTrajectoryIterations[state] = {"C": 0, "D": 0}
+            elif location == 2:
+                self.allTrajectoryIterations[state] = {"C": 0, "R": 0}
+            elif location == 3:
+                self.allTrajectoryIterations[state] = {"C": 0, "D": 0, "R": 0, "L": 0, "U": 0}
+            elif location == 4:
+                self.allTrajectoryIterations[state] = {"C": 0, "L": 0}
+            elif location == 5:
+                self.allTrajectoryIterations[state] = {"C": 0, "U": 0}
 
-            print(state + ": " + str(q) + " " + a)
+            self.allIterationsInOrder.append(stateAndAction)
 
     def qvalue(self, state, action):
         # state is a string representation of a state
@@ -80,12 +64,13 @@ class td_qlearning:
             if int(state[counter]) == 1:
                 self.reward = self.reward - 1
 
-        currentLocation = int(state[0])
-        currentQValue = self.qValuesWithActions[action][currentLocation - 1]
+        currentQValue = self.allTrajectoryIterations[state][action]
 
-        maxQValue = self.findingMaxValue(state)[0]
+        maxQValue = self.findingMaxQValue(state, action)
 
         q = currentQValue + self.alpha * (self.reward + self.gamma * maxQValue - currentQValue)
+
+        self.allTrajectoryIterations[state][action] = q
 
         # Return the q-value for the state-action pair
         return round(q, 6)
@@ -93,9 +78,17 @@ class td_qlearning:
     def policy(self, state):
         # state is a string representation of a state
 
-        a = self.findingMaxValue(state)[1]
+        actionDictionary = self.allTrajectoryIterations[state]
+
+        action = "C"
+
+        maxQValue = -1000000000
+        for keyState in actionDictionary.keys():
+            if maxQValue < actionDictionary[keyState]:
+                action = keyState
+                maxQValue = actionDictionary[keyState]
 
         # Return the optimal action under the learned policy
-        return a
+        return action
 
-td_qlearning = td_qlearning("Example2/trajectory.csv")
+#td_qlearning = td_qlearning("Example1/trajectory.csv")
